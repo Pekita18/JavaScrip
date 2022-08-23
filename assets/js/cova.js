5966f1b774a6c30a1ac3fb92330b8fe599d08a68
Valentin <valentin.reyes.valderrama018@gmail.com>
2022-08-23 17:46:26 -0300

    DIN DOM

---
+++ assets/js/cova.js
@@ -0,0 +1,120 @@
+const artyom = new Artyom();
+
+/*======= ARRAY BASE DE DATOS BOT =========*/
+
+const dialogoBots = [{user: "Hola", bot: "Hola soy Cova"}, {user: "Cómo te llamas", bot: "Mi nombre es Cova"},{user: "Chau", bot: "Hasta luego amigo."}, {user: "Abriendote", bot:"es la unica forma de conocerte.(3:05)"}];
+
+/*======= ANULAR ENVIAR INPUT ========*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('input[type=text]').forEach( node => node.addEventListener('keypress', e => {
+        /*== AND ==*/
+        e.keyCode == 13 && e.preventDefault();
+    }))
+});
+
+/*======= ACTUALIZAR ARRAY ======*/
+
+const subirArray = () => {
+    let objArr = {};
+
+    for ( let i = 0; i < localStorage.length; i++) { 
+        objArr = localStorage.getItem(localStorage.key(i)); 
+        let subirArr = JSON.parse(objArr);
+        dialogoBots.push(subirArr);
+    } 
+}
+
+subirArray();
+
+/*======= VARIABLE PARRAFO =========*/
+
+let p = document.getElementById("bot");
+
+/*======= FUNCION MAYUSCULA PRIMERA LETRA ========*/
+
+const mayuscula = (palabra) => {
+    return palabra.charAt(0).toUpperCase() + palabra.slice(1);
+}
+
+/*======= INICIAR ARTYOM =======*/
+
+let voz = document.getElementById("voz");
+
+voz.onclick = () => {
+    artyom.initialize({
+        lang: "es-ES",
+        continuous: true,
+        listen: true,
+        debug: true,
+        speed: 1
+    });
+}
+
+/*======= FUNCION VOZ BOT =======*/
+
+artyom.addCommands([
+    {
+        indexes:["Desactivar Voz", "quiero enseñar"],
+        action: function(i){
+            if(i==0){
+                artyom.say("Voz Desactivada");
+            }
+            if(i==1){
+                artyom.say("Toca el boton enseñar en la parte superior del menu.")
+            }
+        }
+    }
+]);
+
+artyom.redirectRecognizedTextOutput(function(recognized,isFinal){
+    if(isFinal){
+        let ingresado = recognized;
+        let reconoce = dialogoBots.find((el) => el.user == mayuscula(ingresado));
+        if(reconoce){
+            artyom.say(reconoce.bot);
+            p.innerHTML = "";
+            artyom.fatality();
+        }else{
+            artyom.say("No tengo respuesta para esto, enseñame por favor.")
+            p.innerHTML = `No tengo respuesta! Toca el boton <a href="./enseñar.html"> Enseñar </a>.`
+            artyom.fatality();
+        }
+    }
+});
+
+/*======= FUNCION HABLAR BOT =======*/
+
+const hablarBot = () => {
+
+    /*== Variables ==*/
+    let chatUser = document.getElementById("hablar").value;
+    let comprobacion = dialogoBots.find((el) => el.user == mayuscula(chatUser));
+    const noRes = p.innerHTML = `No tengo respuesta! Toca el boton <a href="./enseñar.html"> Enseñar </a>.`;
+
+    tipo_pokemon(chatUser);
+
+    /*== OR ==*/
+    p.innerHTML = comprobacion.bot || noRes;
+
+}
+
+/*======= BOTON BOT ========*/
+
+let botonBot = document.getElementById("btn");
+
+botonBot.onclick = () => {
+    hablarBot();
+}
+
+const tipo_pokemon = (tipo) => {
+    let bd_type = `https://pokeapi.co/api/v2/type/${tipo}`;
+    fetch(bd_type)
+    .then(resp => resp.json())
+    .then(data => {
+        let aleatorio = Math.floor(Math.random() * (data.pokemon.length));
+        pokemon = data.pokemon[aleatorio].pokemon.name;
+        tipo_pok = data.name;
+        p.innerHTML = `${pokemon} es un pokemon de tipo ${tipo_pok}`
+    })
+}
---
+++ assets/js/enseñar.js
@@ -0,0 +1,37 @@
+/*======= FUNCION MAYUSCULA PRIMERA LETRA ========*/
+
+const mayuscula = (palabra) => {
+    return palabra.charAt(0).toUpperCase() + palabra.slice(1);
+}
+
+/*======= FUNCION SUBIR LOCAL ========*/
+
+const subirLocal = () => {
+    /*== Variables ==*/
+    chatUser = document.getElementById("hablar").value;
+    chatBot = document.getElementById("enseñar").value;
+    const objLocal = {user: mayuscula(chatUser), bot: chatBot};
+    const subirLocal = JSON.stringify(objLocal);
+
+    /*== Storage Local==*/
+    localStorage.setItem(chatUser, subirLocal);        
+}
+
+/*======= FUNCION ENSEÑAR BOT ========*/
+
+const enseñarBot = () => {
+    let p = document.getElementById("bot");
+    p.innerHTML = `Ya lo aprendi, pruebalo en la <a href="./cova.html"> Pagina de Cova </a>!!`;
+    document.getElementById("hablar").value = "";
+    document.getElementById("enseñar").value = "";
+    subirLocal();
+}
+
+/*======= BOTON ENSEÑAR BOT ========*/
+
+let botonBot = document.getElementById("btn");
+
+botonBot.onclick = () => {
+    subirLocal()
+    enseñarBot();
+}
